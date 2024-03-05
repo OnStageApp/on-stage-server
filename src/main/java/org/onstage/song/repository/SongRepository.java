9719c737a7ac199c8b1bd5595d4e8e0d4b7b56af
ToniVinter <tonyivinter@gmail.com>
2024-03-06 00:14:34 +0200

    added search filter on getSongs

--- src/main/java/org/onstage/song/repository/SongRepository.java
+++ src/main/java/org/onstage/song/repository/SongRepository.java
@@ -1,13 +1,18 @@
 package org.onstage.song.repository;
 
 import lombok.RequiredArgsConstructor;
-import org.onstage.song.client.Song;
 import org.onstage.song.model.SongEntity;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
+
+import static org.onstage.song.model.SongEntity.Fields.lyrics;
+import static org.onstage.song.model.SongEntity.Fields.title;
 
 @Component
 @RequiredArgsConstructor
@@ -19,7 +24,16 @@ public class SongRepository {
         return songRepo.findById(id);
     }
 
-    public List<SongEntity> getAll() {
+    public List<SongEntity> getAll(String search) {
+        if (search != null && !search.isEmpty()) {
+            Criteria criteria = new Criteria().orOperator(
+                    Criteria.where(title).regex(search, "i"),
+                    Criteria.where(lyrics).regex(search, "i")
+            );
+            Query query = new Query(criteria);
+            query.with(Sort.by(Sort.Order.asc("title"), Sort.Order.asc("lyrics")));
+            return mongoTemplate.find(query, SongEntity.class);
+        }
         return songRepo.findAll();
     }
 
