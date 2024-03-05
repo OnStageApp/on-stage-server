OnStageApp/event-service

created EventItemEntity + changes to Event apis

src/main/java/org/onstage/event/controller/EventController.java
@@ -2,17 +2,22 @@ package org.onstage.event.controller;
 
 import com.github.fge.jsonpatch.JsonPatch;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.onstage.event.client.Event;
+import org.onstage.event.client.EventOverview;
 import org.onstage.event.model.EventEntity;
 import org.onstage.event.model.mappers.EventMapper;
 import org.onstage.event.service.EventService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("events")
 @RequiredArgsConstructor
+@Slf4j
 public class EventController {
     private final EventService service;
     private final EventMapper mapper;
@@ -23,17 +28,28 @@ public class EventController {
     }
 
     @GetMapping
-    public List<Event> getAll() {
-        return mapper.toApiList(service.getAll());
+    public List<EventOverview> getAll(
+            @RequestParam(required = false) String search,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime endDate) {
+        if (startDate != null || endDate != null) {
+            return mapper.toOverviewList(service.getAllByRange(startDate, endDate));
+        }
+        return mapper.toOverviewList(service.getAll(search));
     }
 
     @PostMapping
-    public String create(@RequestBody final EventEntity event) {
+    public String create(@RequestBody() EventEntity event) {
         return service.create(event).id();
     }
 
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable final String id) {
+        return service.delete(id);
+    }
+
     @PatchMapping("/{id}")
-    public Event patch(@PathVariable String id, @RequestBody JsonPatch jsonPatch){
+    public Event patch(@PathVariable String id, @RequestBody JsonPatch jsonPatch) {
         return mapper.toApi(service.patch(id, jsonPatch));
     }
 }

OnStageApp/on-stage-server

src/main/java/org/onstage/event/controller/EventController.java
@@ -3,11 +3,14 @@ package org.onstage.event.controller;
 import com.github.fge.jsonpatch.JsonPatch;
 import lombok.RequiredArgsConstructor;
 import org.onstage.event.client.Event;
+import org.onstage.event.client.EventOverview;
 import org.onstage.event.model.EventEntity;
 import org.onstage.event.model.mappers.EventMapper;
 import org.onstage.event.service.EventService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -23,17 +26,28 @@ public class EventController {
     }
 
     @GetMapping
-    public List<Event> getAll() {
-        return eventMapper.toApiList(eventService.getAll());
+    public List<EventOverview> getAll(
+            @RequestParam(required = false) String search,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime endDate) {
+        if (startDate != null || endDate != null) {
+            return eventMapper.toOverviewList(eventService.getAllByRange(startDate, endDate));
+        }
+        return eventMapper.toOverviewList(eventService.getAll(search));
     }
 
     @PostMapping
-    public String create(@RequestBody final EventEntity event) {
+    public String create(@RequestBody() EventEntity event) {
         return eventService.create(event).id();
     }
 
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable final String id) {
+        return eventService.delete(id);
+    }
+
     @PatchMapping("/{id}")
-    public Event patch(@PathVariable String id, @RequestBody JsonPatch jsonPatch){
+    public Event patch(@PathVariable String id, @RequestBody JsonPatch jsonPatch) {
         return eventMapper.toApi(eventService.patch(id, jsonPatch));
     }
 }