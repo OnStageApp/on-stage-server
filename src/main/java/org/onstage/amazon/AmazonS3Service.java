67642a56b44e531475f31f2c600c59d775698ddb
emimavinter <caba.emimaa@gmail.com>
2024-09-10 16:58:05 +0300

    revert aws s3 service changes

--- src/main/java/org/onstage/amazon/AmazonS3Service.java
+++ src/main/java/org/onstage/amazon/AmazonS3Service.java
@@ -28,12 +28,14 @@ public class AmazonS3Service {
     @Value("${clound.aws.s3.bucket}")
     private String bucketName;
 
-    private final Integer MAX_DIMENSION = 300;
-    private final Integer THUMBNAIL_DIMENSION = 80;
+    private final Integer DEFAULT_WIDTH = 200;
+    private final Integer DEFAULT_HEIGHT = 200;
+    private final Integer THUMBNAIL_WIDTH = 50;
+    private final Integer THUMBNAIL_HEIGHT = 50;
 
     public void putObject(byte[] image, String key, String contentType) {
         try {
-            byte[] resizedImage = resizeImage(image, getFormatFromContentType(contentType), MAX_DIMENSION);
+            byte[] resizedImage = resizeImage(image, getFormatFromContentType(contentType), DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
             InputStream inputStream = new ByteArrayInputStream(resizedImage);
             ObjectMetadata metadata = new ObjectMetadata();
@@ -48,46 +50,31 @@ public class AmazonS3Service {
     }
 
     public byte[] getObject(String key) {
+        return getObject(key, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+
+    public byte[] getObject(String key, int width, int height) {
         try {
             S3Object s3Object = amazonS3.getObject(bucketName, key.toLowerCase());
-            return IOUtils.toByteArray(s3Object.getObjectContent());
+            byte[] originalImage = IOUtils.toByteArray(s3Object.getObjectContent());
+            return resizeImage(originalImage, getFormatFromContentType(s3Object.getObjectMetadata().getContentType()), width, height);
         } catch (AmazonServiceException | IOException e) {
             log.error("Failed to get image with key {} with error {}", key, e.getMessage());
-            return null; // Consider throwing a custom exception instead
+            return null;
         }
     }
 
     public byte[] getThumbnail(String key) {
-        try {
-            S3Object s3Object = amazonS3.getObject(bucketName, key.toLowerCase());
-            byte[] originalImage = IOUtils.toByteArray(s3Object.getObjectContent());
-            return resizeImage(originalImage, getFormatFromContentType(s3Object.getObjectMetadata().getContentType()), THUMBNAIL_DIMENSION);
-        } catch (AmazonServiceException | IOException e) {
-            log.error("Failed to get thumbnail with key {} with error {}", key, e.getMessage());
-            return null; // Consider throwing a custom exception instead
-        }
+        return getObject(key, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT);
     }
 
-    private byte[] resizeImage(byte[] originalImage, String format, int maxDimension) throws IOException {
+    private byte[] resizeImage(byte[] originalImage, String extension, int width, int height) throws IOException {
         BufferedImage img = ImageIO.read(new ByteArrayInputStream(originalImage));
-        int originalWidth = img.getWidth();
-        int originalHeight = img.getHeight();
-
-        int newWidth, newHeight;
-        if (originalWidth > originalHeight) {
-            newWidth = maxDimension;
-            newHeight = (originalHeight * maxDimension) / originalWidth;
-        } else {
-            newHeight = maxDimension;
-            newWidth = (originalWidth * maxDimension) / originalHeight;
-        }
-
-        java.awt.Image resizedImage = img.getScaledInstance(newWidth, newHeight, java.awt.Image.SCALE_SMOOTH);
-        BufferedImage bufferedResizedImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+        java.awt.Image resizedImage = img.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH);
+        BufferedImage bufferedResizedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         bufferedResizedImage.getGraphics().drawImage(resizedImage, 0, 0, null);
-
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(bufferedResizedImage, format, baos);
+        ImageIO.write(bufferedResizedImage, extension, baos);
         return baos.toByteArray();
     }
 
