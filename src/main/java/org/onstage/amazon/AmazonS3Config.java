216aad4ed3c8e4ce9aeb9cc73a7a52387e105b5d
ToniVinter <tonyivinter@gmail.com>
2024-09-29 09:44:35 +0300

    changes to aws s3 config

--- src/main/java/org/onstage/amazon/AmazonS3Config.java
+++ src/main/java/org/onstage/amazon/AmazonS3Config.java
@@ -2,12 +2,9 @@ package org.onstage.amazon;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.secretsmanager.AWSSecretsManager;
-import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,25 +12,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AmazonS3Config {
 
-    // Use environment variables, system properties, or ~/.aws/config for the region if available
-    @Value("${cloud.aws.region:#{systemEnvironment['AWS_REGION'] ?: 'eu-central-1'}}")
-    private String region;
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
 
     @Bean
     public AmazonS3 amazonS3() {
         return AmazonS3ClientBuilder.standard()
-                // Use the default credentials provider chain (environment variables, .aws/credentials, etc.)
-                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
-                .withRegion(Regions.fromName(region)) // Dynamically set the region
-                .build();
-    }
-
-    @Bean
-    public AWSSecretsManager awsSecretsManager() {
-        return AWSSecretsManagerClientBuilder.standard()
-                // Use default credentials provider chain for secrets manager
-                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
-                .withRegion(Regions.fromName(region)) // Dynamically set the region
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials(accessKey, secretKey)))
+                .withRegion(Regions.EU_CENTRAL_1)
                 .build();
     }
 }
