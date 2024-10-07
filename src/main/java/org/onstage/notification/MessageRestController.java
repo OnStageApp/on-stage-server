a42d07d64accb382fc35a2841fd9a5057255eff7
ToniVinter <tonyivinter@gmail.com>
2024-10-07 12:17:53 +0300

    update controller for testing

--- src/main/java/org/onstage/notification/MessageRestController.java
+++ src/main/java/org/onstage/notification/MessageRestController.java
@@ -3,6 +3,7 @@ package org.onstage.notification;
 
 import lombok.RequiredArgsConstructor;
 import org.onstage.notification.model.WebSocketMessage;
+import org.onstage.rabbitmq.UserProducer;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 @RequiredArgsConstructor
 public class MessageRestController {
-    private final WebSocketMessageService webSocketMessageService;
+    private final UserProducer userProducer;
 
     @PostMapping("/send-message")
     public void sendMessage(@RequestBody WebSocketMessage message) {
-        System.out.println("Received message via REST: " + message);
-        webSocketMessageService.sendMessage("/topic/messages", message);
+        System.out.println("Sent message via REST: " + message);
+        userProducer.sendMessage(message.content());
     }
 }
