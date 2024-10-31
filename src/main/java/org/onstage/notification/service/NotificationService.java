cdc6e67eb7920738155b2d82145f5242b00bbf19
emimavinter <caba.emimaa@gmail.com>
2024-10-31 11:28:35 +0200

    added pushToken on sendNotificationToUser

--- src/main/java/org/onstage/notification/service/NotificationService.java
+++ src/main/java/org/onstage/notification/service/NotificationService.java
@@ -2,6 +2,7 @@ package org.onstage.notification.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.onstage.common.service.FirebaseService;
 import org.onstage.device.service.DeviceService;
 import org.onstage.notification.action.GetNotificationsAction;
 import org.onstage.notification.client.NotificationFilter;
@@ -9,6 +10,7 @@ import org.onstage.notification.client.NotificationStatus;
 import org.onstage.notification.client.NotificationType;
 import org.onstage.notification.model.Notification;
 import org.onstage.notification.repository.NotificationRepository;
+import org.onstage.reminder.model.Reminder;
 import org.onstage.socketio.service.SocketIOService;
 import org.springframework.stereotype.Component;
 
@@ -24,6 +26,7 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
     private final SocketIOService socketIOService;
     private final DeviceService deviceService;
+    private final FirebaseService firebaseService;
 
     public List<Notification> getAllNotifications(NotificationFilter filter) {
         return getNotificationsAction.execute(filter);
@@ -37,7 +40,12 @@ public class NotificationService {
                 .description(description)
                 .build();
         notificationRepository.save(notification);
-        deviceService.getAllLoggedDevices(userId).forEach(device -> socketIOService.sendToUser(notification.userId(), device.getId(), NOTIFICATION, null));
+
+        deviceService.getAllLoggedDevices(userId).forEach(device -> {
+            socketIOService.sendToUser(notification.userId(), device.getId(), NOTIFICATION, null);
+            firebaseService.sendNotification(Reminder.builder().text(description).build(), device.getPushToken());
+
+        });
         log.info("New notification {} has been sent to user {}", type, notification.userId());
     }
 }
