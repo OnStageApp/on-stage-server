df136ac89421e6239a5b3aec574bf0d7f05e67b1
emimavinter <caba.emimaa@gmail.com>
2024-11-01 16:59:09 +0200

    send push only if push token exists

--- src/main/java/org/onstage/notification/service/PushNotificationService.java
+++ src/main/java/org/onstage/notification/service/PushNotificationService.java
@@ -4,24 +4,26 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PushNotificationService {
 
-    public void sendPushNotification(String title, String body, String userDeviceToken) {
+    public void sendPushNotification(String title, String body, String pushToken) {
         Notification pushNotification = Notification.builder()
                 .setTitle(title)
                 .setBody(body)
                 .build();
 
-        sendPush(pushNotification, userDeviceToken);
+        if (Strings.isNotEmpty(pushToken))
+            sendPush(pushNotification, pushToken);
     }
 
-    private void sendPush(Notification pushNotification, String userDeviceToken) {
+    private void sendPush(Notification pushNotification, String pushToken) {
         Message message = Message.builder()
                 .setNotification(pushNotification)
-                .setToken(userDeviceToken)
+                .setToken(pushToken)
                 .build();
 
         try {
